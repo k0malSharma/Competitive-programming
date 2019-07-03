f3b389ecb4f3b0cb73ea3cc9ee63724eb299a24d
k0malSharma <52309262+k0malSharma@users.noreply.github.com>
2019-07-03 20:06:54 +0530

    Solution to Codechef problem FLOW016
    Link to the problem https://www.codechef.com/problems/FLOW016

---
+++ FLOW016.py
@@ -0,0 +1,11 @@
+def gcd(x,y):
+    if(x==0):
+        return(y)
+    return gcd(y%x,x)
+
+    
+for _ in range(int(input())):
+    a, b = [int(i) for i in input().split()]
+    c=gcd(a,b)
+    print(c,(a*b)//c)
+ 
