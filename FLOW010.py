39fb899325035302f9d4f9aa9f90c0a6625a2d24
k0malSharma <52309262+k0malSharma@users.noreply.github.com>
2019-07-28 22:31:54 +0530

    Solution to Codechef problem FLOW010
    Link to the problem https://www.codechef.com/problems/FLOW010

---
+++ FLOW010.py
@@ -0,0 +1,10 @@
+for _ in range(int(input())):
+    ch=input()
+    if (ch=="B" or ch=="b"):
+        print("BattleShip")
+    if (ch=="C" or ch=="c"):
+        print("Cruiser")
+    if (ch=="D" or ch=="d"):
+        print("Destroyer")
+    if (ch=="F" or ch=="f"):
+        print("Frigate")
